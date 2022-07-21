1e57344faef95675ae7c0570cf74ba6544980be1
Tom LaChance <guitarlanguageseries@gmail.com>
2022-07-21 12:57:02 -0500

    read the files and have a more dynamic getStaticPaths()

---
+++ pages/posts/[slug].js
@@ -0,0 +1,42 @@
+import Head from "next/head";
+import { getPost, getSlugs } from "../../lib/post";
+
+export async function getStaticPaths() {
+  const slugs = await getSlugs();
+  return {
+    paths: slugs.map((slug) => ({
+      params: { slug },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  console.log('[ PostPage ] getStaticProps()');
+  const post = await getPost(slug);
+
+  return {
+    props: { post },
+  };
+}
+
+function PostPage({ post }) {
+  
+  return (
+    <>
+      <Head>
+        <title>{post.title} - My Blog</title>
+        <meta name=" PostPage" value=" PostPage" />
+      </Head>
+      <main>
+        <h1>{post.title}</h1>
+        <p>published on: {post.date}</p>
+        <article dangerouslySetInnerHTML={{__html: post.body}} />
+        
+      </main>
+    </>
+    
+  );
+}
+
+export default PostPage;
